1b0da3b4f5967d29b81f83e7afadb78ca55da9a3
Thespartann <danilastefan69@yahoo.ro>
2019-10-27 14:11:36 +0000

    bacon: Magma bringup
    Signed-off-by: Thespartann <danilastefan69@yahoo.ro>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/xtended_bacon.mk
+    $(LOCAL_DIR)/magma_bacon.mk
 
 COMMON_LUNCH_CHOICES := \
-    xtended_bacon-user \
-    xtended_bacon-userdebug \
-    xtended_bacon-eng
+    magma_bacon-user \
+    magma_bacon-userdebug \
+    magma_bacon-eng
